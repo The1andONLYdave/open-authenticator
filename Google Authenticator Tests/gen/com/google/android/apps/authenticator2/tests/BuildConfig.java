bc6ff4cd894eaab66f4201dc42a728fe71d79e97
David-Lee Kulsch <The1andONLYdave@googlemail.com>
2014-01-17 13:11:37 +0100

    1
    2

--- Google Authenticator Tests/gen/com/google/android/apps/authenticator2/tests/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.google.android.apps.authenticator2.tests;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
